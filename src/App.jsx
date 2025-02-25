5b2947fd7dd14646eb8acb0740e7d34e8675fb49
Hemant Dhote <hemantdhote76@gmail.com>
2025-02-26 02:59:35 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const App = () => {
-  const [toggle, settoggle] = useState(false);
+  const [toggle, setToggle] = useState(false);
 
-
-  const visibilityHandler=()=>{
-    settoggle(!toggle);
-
+  const visibilityHandler = () => {
+    setToggle(!toggle);
   };
-  
-
 
   return (
-    <div>
-      <div className=' w-full flex flex-col items-center p-10 gap-3'>
-        <button className='px-3 py-2 bg-green-600 rounded-md text-white'>Show Text</button>
-        <p className='text-md font-semibold capitalize'>this is the text and you can toggele this</p>
-      </div>
-
-      
+    <div className="w-full flex flex-col items-center p-10 gap-3">
+      <button onClick={visibilityHandler} className="px-3 py-2 bg-green-600 rounded-md text-white">
+        {toggle ? 'Hide Text' : 'Show Text'}
+      </button>
+      {toggle && <p className="text-md font-semibold capitalize">This is the text and you can toggle this</p>}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
